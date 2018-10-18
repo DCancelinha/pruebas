a72e3b09ef98ce38c40018d9cade7e1289051948
DCancelinha <44257743+DCancelinha@users.noreply.github.com>
2018-10-18 10:41:37 +0200

    Añado ficheros java con UPLOAD

---
+++ Animal.java
@@ -0,0 +1,36 @@
+/**
+*clase que crea un animal, dada su edad y su nombre
+*
+*@author Diego
+*@version 1.0
+*/
+
+//Definición de la Clase Animal
+  public class Animal{
+  	//Definición de los Atributos
+  	private String nombre;
+  	private int edad;
+
+  	//Definición de los Métodos
+
+  	public void nace(){
+  		System.out.print("Hola, he nacido. ");
+  	}
+
+  	public String getNombre(){
+  		return nombre;
+  	}
+
+  	public int getEdad(){
+  		return edad;
+  	}
+
+  	public void setNombre(String nombre){
+  		this.nombre = nombre;
+  	}
+
+  	public void setEdad(int edad){
+  		this.edad = edad;
+  	}
+  	
+  }
